cffa6870086b29716ac7e94110e04e1ad376d550
Nikhilsharma97 <nikhil.sharma1@kelltontech.com>
2021-01-18 12:53:51 +0530

    NAME ADDED IN MAIN

--- src/main/java/com/nikhil/demo/LmsApplication.java
+++ src/main/java/com/nikhil/demo/LmsApplication.java
@@ -3,11 +3,27 @@ package com.nikhil.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+/*
+ * 
+ *  MADE BY NIKHIL SHARAMA 6550 KELLTON TECH SOL.
+ * 
+ * 
+ * 
+ * 
+ */
+
 @SpringBootApplication
 public class LmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
+	
+	
+	
+	
+	
 
 }
